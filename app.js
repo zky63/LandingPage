e3a52fd03789fd583e185a2dfa9dd1049fc91854
zky63 <76175779+zky63@users.noreply.github.com>
2020-12-20 15:22:13 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-d
+const reqButton = document.querySelector('#inputButton');
+const input = document.querySelector('#email');
+const errorText = document.querySelector('#errorText');
+
+reqButton.addEventListener('click', function(){
+    const inputValue = input.value;
+    console.log(inputValue);
+    if(!inputValue){
+        errorText.innerText = 'Oops! Please add your email';
+        errorText.classList.add('textContainer__error--visible');
+    }
+    else if (!(inputValue.includes('@')) && inputValue){
+        errorText.innerText = 'Oops! Please check your email';
+        errorText.classList.add('textContainer__error--visible');
+    }
+    else if (inputValue && inputValue.includes('@')) {
+        errorText.classList.toggle('textContainer__error--visible');
+    }
+})
+
+
+
+
